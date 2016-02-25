61cf9ca496ef9962262514d8427b96d147e11a8f
betterin30days <leecostello1@gmail.com>
2016-02-24 19:54:45 -0500

    Initial Commit

---
+++ Food Tracker/MealTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MealTableViewCell.swift
+//  Food Tracker
+//
+//  Created by Lee Costello on 2/18/16.
+//  Copyright © 2016 Lee Costello. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
